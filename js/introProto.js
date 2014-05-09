f30e93f675e144385fb464885dd7bc5af6b6135d
Tyler Chadwick <chadwickthebold@gmail.com>
2014-05-08 23:29:10 -0400

    added basic hover behavior

--- js/introProto.js
+++ js/introProto.js
@@ -6,5 +6,26 @@
 			$q2 = $('.q2'),
 			$q3 = $('.q3'),
 			$q4 = $('.q4'),
-			$quadContainer = $('.quadWrapper');
+			$quadContainer = $('.quadWrapper'),
+			$quads = $('.quad');
+
+			$quads.on('mouseenter.quad', function(){
+				$quads.removeClass('quadActive');
+				$(this).velocity({
+					width: "+=5em",
+					height: "+=5em"
+				}).addClass('quadActive');
+				$(this).find('.desc').velocity({
+					'lineHeight': '+=5em',
+					'width': '+=5em'
+				})
+				$quads.not(this).velocity({
+					width: "-=5em",
+					height: "-=5em"
+				})
+			}).on('mouseleave.quad', function() {
+				$(this).velocity('reverse');
+				$(this).find('.desc').velocity('reverse');
+				$quads.not(this).velocity('reverse');
+			});
 }())
